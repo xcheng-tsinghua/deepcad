7611e36f60d9739775aeaad4c48eab263b77d2eb
xcheng-tsinghua <786682191@qq.com>
2025-08-24 13:47:42 +0800

    change

--- config/configAE.py
+++ config/configAE.py
@@ -12,14 +12,15 @@ class ConfigAE(object):
         parser = argparse.ArgumentParser()
 
         parser.add_argument('--proj_dir', type=str, default="proj_log", help="data save dir")
+        parser.add_argument('--exp_name', type=str, default='newDeepCAD', help="name of this experiment")
         parser.add_argument('--data_root', type=str, default="data", help="path to source data folder")
-        parser.add_argument('--exp_name', type=str, default='test', help="name of this experiment")
-        parser.add_argument('--gpu_ids', type=str, default='0', help="gpu to use, e.g. 0  0,1,2. CPU not supported.")
 
+        parser.add_argument('--nr_epochs', type=int, default=2000, help="total number of epochs to train")
         parser.add_argument('--batch_size', type=int, default=512, help="batch size")
+
+        parser.add_argument('--gpu_ids', type=str, default='0', help="gpu to use, e.g. 0  0,1,2. CPU not supported.")
         parser.add_argument('--num_workers', type=int, default=8, help="number of workers for data loading")
 
-        parser.add_argument('--nr_epochs', type=int, default=1000, help="total number of epochs to train")
         parser.add_argument('--lr', type=float, default=1e-3, help="initial learning rate")
         parser.add_argument('--grad_clip', type=float, default=1.0, help="initial learning rate")
         parser.add_argument('--warmup_step', type=int, default=2000, help="step size for learning rate warm up")
